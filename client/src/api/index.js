96ae267f1497cfbc97ebacb8bdee0bc39d21a977
Manreet30 <kaurmanreet0408@gmail.com>
2025-05-06 23:58:42 -0400

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-project-back.onrender.com';
+const url = 'https://memories-project-back.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
